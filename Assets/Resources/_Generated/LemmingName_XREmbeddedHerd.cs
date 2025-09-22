7052e0c884bfbc6d02397381d95ee061741931c8
JustinBerryArt <55406658+JustinBerryArt@users.noreply.github.com>
2025-09-22 09:24:56 -0400

    update

---
+++ Assets/Resources/_Generated/LemmingName_XREmbeddedHerd.cs
@@ -0,0 +1,21 @@
+namespace Lemmings
+{
+    public enum LemmingName_XREmbeddedHerd
+    {
+        None,
+        Right_MiddleTip,
+        Right_LittleTip,
+        Left_RingTip,
+        XRHand_IndexTip,
+        Right_Wrist,
+        Right_RingTip,
+        Left_Wrist,
+        Head,
+        XRHand_ThumbTip,
+        Chest,
+        Left_MiddleTip,
+        Left_LittleTip,
+        Left_ThumbTip,
+        Left_IndexTip,
+    }
+}
